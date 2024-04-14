3744cdd8df5cd83445c5789f46ae3c455e1a80a4
viktorialoza <161321385+viktorialoza@users.noreply.github.com>
2024-04-14 15:46:49 +0300

    created a project

---
+++ Assets/InputsysScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class InputsysScript : MonoBehaviour
+{
+    private void Update()
+    {
+        if (Input.anyKeyDown)
+        {
+            string keyPressed = Input.inputString;
+            Debug.Log("pressed key" + keyPressed);
+        }
+    }
+}
+   
+  
